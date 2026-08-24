88ad1acbc6353fb3f9b20721d3666412974bae69
Rohit Singh Gusain <rohitgusain9930@gmail.com>
2026-08-24 21:59:52 +0530

    Fix string formatting in deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(){
-  echo ========= creating container===============
+  echo "========= creating container==============="
   sh 'docker run -d -p 80:80 --name my_container rohitsinghgusain30/my_nihon_app:latest '
-  echo =========== container created successfully===========
+  echo "=========== container created successfully==========="
 }
