02184cf5042000ef9f5bb257919c4d672713eb2d
Rohit Singh Gusain <rohitgusain9930@gmail.com>
2026-08-24 22:04:22 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,7 @@
 def call(){
   echo "========= creating container==============="
+  sh 'docker stop my_container'
+  sh 'docker rm my_container'
   sh 'docker run -d -p 80:80 --name my_container rohitsinghgusain30/my_nihon_app:latest '
   echo "=========== container created successfully==========="
 }
